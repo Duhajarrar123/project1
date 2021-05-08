8ebc5178eb3ec79fe3e7e93cd0d247957217322b
Duha Jarrar <54679733+duhajarrar@users.noreply.github.com>
2021-05-09 00:00:31 +0300

    duha

--- Duha1171742.py
+++ Duha1171742.py
@@ -21,8 +21,6 @@ for ind in range(len(imgsList)):
     gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
     cv2.imwrite(path+"/GreyImg.png", gray)
     # Apply Threshold
-    thresh = np.array(255 * (gray / 255) ** 1 , dtype='uint8')
-    cv2.imwrite(path+"/greyImgCorrelation.png", thresh)
     ret,thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+ cv2.THRESH_OTSU)
     cv2.imwrite(path+'/ThresholdOtsu.png',thresh)
     # Apply Horizontal Projection
@@ -35,21 +33,37 @@ for ind in range(len(imgsList)):
     b=0
     c=0
     yp=0
+    num=0
+    s=0
     # Change Scale of Horizontal Projection and Draw It's Result
     for row in range(height):
-        if round(2*int(horizontal_projection[row]*width/height),-3)>0:
-            if abs(yp-row) > 90:
-               a=random.randint(0,255)
-               b=random.randint(0,255)
-               c=random.randint(0,255)
-               yp = row
-            cv2.line(imgcpy, (0,row), (round(9*int(horizontal_projection[row]*width/height),-3),row) ,(a, b,c), 5)
-            cv2.line(whiteImg, (0, row), (round(9 * int(horizontal_projection[row] * width / height), -3), row), (a, b, c), 5)
-            #print(int(horizontal_projection[row]*width/height))
+        if round(2 * int(horizontal_projection[row] * width / height), -3) > 0:
+            if num == 0:
+                s += 1
+                cv2.putText(pic, text=str("Line"), org=(50, row + 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2,color=(255, 0, 0), thickness=3, lineType=cv2.LINE_AA)
+                num = 1
+            if abs(yp - row) > 90:
+                a = random.randint(0, 255)
+                b = random.randint(0, 255)
+                c = random.randint(0, 255)
+                yp = row
+                num = 0
+            cv2.line(imgcpy, (0, row), (round(9 * int(horizontal_projection[row] * width / height), -3), row),(a, b, c), 5)
+            cv2.line(whiteImg, (0, row), (round(9 * int(horizontal_projection[row] * width / height), -3), row),(a, b, c), 5)
+        else:
+            cv2.line(whiteImg, (0, row), (round(9 * int(horizontal_projection[row] * width / height), -3), row), (255, 255, 255), 5)
+    cv2.putText(pic, text=str("This image have a " + str(s-1) + " Lines"), org=(700, row - 120),fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=4, color=(255, 0, 0), thickness=3, lineType=cv2.LINE_AA)
+
+
+
+
+    #print(int(horizontal_projection[row]*width/height))
     cv2.imwrite(path+"/copyImage.png", imgcpy)
-    DILATE_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (600, 7))
-    whiteImg = cv2.morphologyEx(whiteImg, cv2.MORPH_ERODE, DILATE_kernel, iterations=3)
+    ERODE_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (800, 7))
+    whiteImg = cv2.morphologyEx(whiteImg, cv2.MORPH_ERODE, ERODE_kernel, iterations=3)
     cv2.imwrite(path+"/whiteImgCon.png", whiteImg)
+
+
     # Merge The Original Image With Horizontal Projection Lines Result
     res = cv2.addWeighted(pic,0.7,whiteImg,0.3,0)
     cv2.imwrite(path+"/res_2021.png", res)
